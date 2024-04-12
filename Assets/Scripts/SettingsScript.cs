dc2fdc908e2e4896c45661805cc62d88ccb99b3a
KopIAkash1 <andreymart01@gmail.com>
2024-04-13 01:14:22 +0300

    volume change added

--- Assets/Scripts/SettingsScript.cs
+++ Assets/Scripts/SettingsScript.cs
@@ -15,18 +15,23 @@ public class SettingsScript : MonoBehaviour
     public TextMeshProUGUI resText;
     public TextMeshProUGUI qualityText;
     public TextMeshProUGUI FSRText;
+    public Slider volumeSlider;
     void Start(){
         _resolution = Screen.resolutions;
         resText.text = Screen.currentResolution.width.ToString() + "X" + Screen.currentResolution.height.ToString();
         qualityText.text = _qualityPreset[2];
         FSRText.text = _FSRquality[0];
+        volumeSlider.value = AudioListener.volume;
+    }
+    public void changeVolume(){
+        AudioListener.volume = (float) volumeSlider.value;
     }
     public void ChangeValueLeft(int type){
         switch(type){
             case 0:
             print(Screen.currentResolution);
                 for(int i = _resolution.Length-1; i >= 0; i--){
-                    if(Screen.currentResolution.height * Screen.currentResolution.width > _resolution[i].height * _resolution[i].width){
+                    if(Screen.currentResolution.height * Screen.currentResolution.width > _resolution[i].height * _resolution[i].width && Is16By9(_resolution[i].width,_resolution[i].height)){
                         Screen.SetResolution(_resolution[i].width,_resolution[i].height,true);
                         resText.text = _resolution[i].width.ToString() + "X" + _resolution[i].height.ToString();
                         break;
@@ -40,7 +45,7 @@ public class SettingsScript : MonoBehaviour
             case 0:
             print(Screen.currentResolution);
                 for(int i = 0; i < _resolution.Length; i++){
-                    if(Screen.currentResolution.height * Screen.currentResolution.width < _resolution[i].height * _resolution[i].width){
+                    if(Screen.currentResolution.height * Screen.currentResolution.width < _resolution[i].height * _resolution[i].width && Is16By9(_resolution[i].width,_resolution[i].height)){
                         Screen.SetResolution(_resolution[i].width,_resolution[i].height,true);
                         resText.text = _resolution[i].width.ToString() + "X" + _resolution[i].height.ToString();
                         break;
@@ -48,5 +53,10 @@ public class SettingsScript : MonoBehaviour
                 }
             break;
         }
+    }
+    bool Is16By9(int width, int height)
+    {
+        float aspectRatio = (float)width / height;
+        return Mathf.Approximately(aspectRatio, 16f / 9f);
     }
 }
