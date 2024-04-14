534cb15e88ece1f32f97a76d335413b528f9d2e9
OverHome <mark.kozirev@gmail.com>
2024-04-14 22:16:59 +0300

    add pre schene

--- Assets/Scripts/Feeding.cs
+++ Assets/Scripts/Feeding.cs
@@ -7,6 +7,10 @@ using UnityEngine.SceneManagement;
 public class Feeding : MonoBehaviour
 {
     [SerializeField] private Quest quest;
+    [SerializeField] private float timing1;
+    [SerializeField] private float timing2;
+    [SerializeField] private int sceneId;
+    [SerializeField] private bool load;
 
     private void Start()
     {
@@ -23,9 +27,9 @@ public class Feeding : MonoBehaviour
 
     IEnumerator Some1()
     {
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(timing1);
         QuestSystem.Instance.NextQuest();
-        yield return new WaitForSeconds(20);
-        SceneManager.LoadScene(1);
+        yield return new WaitForSeconds(timing2);
+        if(load)SceneManager.LoadScene(sceneId);
     }
 }
